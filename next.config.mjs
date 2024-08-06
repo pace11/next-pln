5f9ba99246d163eb362098c001f2689076c23a42
pace11 <ryanjoker87@gmail.com>
2024-08-07 02:47:02 +0700

    feat: update

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["pln.pace-unv.cloud", "www.infouip3bs.com", "127.0.0.1"],
+    domains: ["pln.pace-unv.cloud", "www.infouip3bs.com", "service.infouip3bs.com", "127.0.0.1"],
   },
 };
 
